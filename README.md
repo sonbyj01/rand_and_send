c57bc5ccb263e0c4ad135c3a9500b1db6cb69017
H Son <sonbyj01@gmail.com>
2020-06-08 18:24:29 -0400

    i messed up before...

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rand[om] and Send 
+Script written for Yale CCM Daily Prayer Requests
+
+The script will (*soon) randomly pick an email from a list and send a Google Form link to that email. 
+The objective is to maintain secrecy on who fills out the Google Form. A cron tab script will be created
+soon so it will send at the same time daily. I will also add an error checking mechanism just in case
+the email fails the first time. 
+
+- [x] send an email
+- [x] randomly pick an email from list (waiting for the list...)
+- [x] error checking just in case failed to connect to socket 
+- [ ] crontab script
+
+I'll make a more comprehensive README.md when I'm done and I feel like it...
---
+++ rand_and_send.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import smtplib
+import ssl
+import random
+import pickle
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+class RandAndSend:
+    def __init__(self, addresses_text):
+        # loads existing pickle file that contains updated list of addresses to choose from
+        # otherwise create a new list of addresses from text file specified
+        try:
+            with open('data.pickle', 'rb') as f:
+                self.data = pickle.load(f)
+        except FileNotFoundError as fnf:
+            print(fnf)
+            self.data = []
+            with open(addresses_text, 'r') as f:
+                content = f.readlines()
+                self.data = [x.strip() for x in content]
+
+    def run(self):
+        self._send(self._select())
+        with open('data.pickle', 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def _select(self):
+        try:
+            random_number = random.randint(0, len(self.data) - 1)
+            return self.data.pop(random_number)
+        except (IndexError, ValueError) as err:
+            print(err)
+            exit()
+
+    def _send(self, to_email):
+        self.from_email = ''
+        self.from_password = ''
+        self.from_port = 465  # SSL
+
+        # set up email packet
+        self.message = MIMEMultipart('alternative')
+        self.message['Subject'] = 'Daily Prayer Request'
+        self.message['From'] = 'Yale CCM'
+        self.message['To'] = to_email
+
+        # set up message
+        self.text = """\
+        Hey you, today's your day!
+        Fill the Google Form: 
+        https://forms.gle/DtWc45XXhaznpS939
+        """
+
+        self.part = MIMEText(self.text, 'plain')
+        self.message.attach(self.part)
+
+        # create a secure SSL context
+        self.context = ssl.create_default_context()
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', self.from_port, context=self.context) as server:
+                server.login(self.from_email, self.from_password)
+                server.sendmail(self.from_email, to_email, self.message.as_string())
+        except:
+            time.sleep(5)
+            self._send(to_email)
+
+
+def main():
+    RandAndSend('file.txt').run()
+
+
+if __name__ == '__main__':
+    main()
