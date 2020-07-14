2d56275fec2bb13d8f09fddab460a423f5ec6c8d
BY Son <sonbyj01@gmail.com>
2020-07-14 18:28:05 -0400

    fixed path

--- script.sh
+++ script.sh
@@ -5,19 +5,19 @@
 
 # add script to crontab to run every Wednesday at 10:00 am:
 # $ crontab -e
-# 0 10 * * 3 /home/helen/projects/prayer_request/script.sh
+# 0 10 * * 3 /home/helen/projects/rand_and_send/script.sh
 
 # sources the virtual environment needed to run the program
-source /home/helen/projects/prayer_request/venv/bin/activate
+source /home/helen/projects/rand_and_send/venv/bin/activate
 
 # moves into project directory
-cd /home/helen/projects/prayer_request
+cd /home/helen/projects/rand_and_send
 
 # turns to executable
-chmod +x /home/helen/projects/prayer_request/rand_and_send.py
+chmod +x /home/helen/projects/rand_and_send/rand_and_send.py
 
 # runs the program
-/home/helen/projects/prayer_request/rand_and_send.py
+/home/helen/projects/rand_and_send/rand_and_send.py
 
 # deactivates the virtual environment
 deactivate
